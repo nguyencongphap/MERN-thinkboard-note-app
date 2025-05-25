dc3aedd65ba8568f57987726a57ca9c8e9ed4bd7
PHAP_LAPTOP\ncpha <nguyencongphap@yahoo.com>
2025-05-25 07:20:34 -0700

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,19 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); // dir path of "backend" dir
 
-// this cors middleware must be before rateLimiter middleware because we want cors to be set up before rateLimiter sends rate limit response back to client
-app.use(cors({ origin: ["http://localhost:5173"] }));
+// In dev, we use 2 domains backend and frontend. In prod, we only have 1 for both.
+if (process.env.NODE_ENV !== "production") {
+  // this cors middleware must be before rateLimiter middleware because we want cors to be set up before rateLimiter sends rate limit response back to client
+  app.use(cors({ origin: ["http://localhost:5173"] }));
+}
 
 // we use ".use" to add a middleware
 // middleware that allows us to get access to the json body of the request
@@ -29,6 +34,18 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  // specify the path of the frontend folder
+  // This means serve our built (optimized) react app as a static asset
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // if we get a request to anything other than "/api/notes" specified above, then we serve our react app
+  app.get("*", (req, res) => {
+    // serve the starting point of our react app, the index.html file
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 await connectDB(); // finish connecting to db successfully before running the server
 app.listen(PORT, () => {
